dd1a2091dda1fe063f083d4fdb3fb59006d21909
antaresofficial <antaresofficial@icloud.com>
2022-04-12 15:52:59 +0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
+import { Button, Input, Space, Table } from "antd";
+import Column from "antd/lib/table/Column";
 import React, { useCallback, useState } from "react";
 import xlsxParser from "xlsx-parse-json";
 
 export const App = () => {
-  const [isReading, setIsReading] = useState(false);
+  const [data, setData] = useState([]);
 
-  const handleFileChange = useCallback((e: any) => {
-    const file = e.target.files[0];
-    xlsxParser.onFileSelection(file).then((data: any) => {
-      var parsedData = data;
-
-      console.log(parsedData);
-      setIsReading(true);
-    });
-  }, []);
+  const handleFileChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files![0];
+      xlsxParser.onFileSelection(file).then((data: any) => {
+        var parsedData: any = Object.entries(data)[0][1];
+        setData(parsedData);
+      });
+    },
+    []
+  );
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <input type="file" accept=".xls, .xlsx" onChange={handleFileChange} />
-      {isReading ? <p>Reading...</p> : null}
+      <Input type="file" accept=".xls, .xlsx" onChange={handleFileChange} />_
+      {data.length ? (
+        <Table dataSource={data}>
+          <Column title="Имя" dataIndex="Имя" key="Имя" />
+          <Column title="Фамилия" dataIndex="Фамилия" key="Фамилия" />
+          <Column title="Отчество" dataIndex="Отчество" key="Отчество" />
+          <Column
+            title="Номер телефона"
+            dataIndex="Номер телефона"
+            key="Номер телефона"
+          />
+          <Column
+            title="Действие"
+            key="Действие"
+            render={() => (
+              <Space size="middle">
+                <Button>Invite</Button>
+                <Button>Delete</Button>
+              </Space>
+            )}
+          />
+        </Table>
+      ) : null}
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-
+import "antd/dist/antd.css";
 import "./styles.css";
 
 const container: any = document.getElementById("app");
